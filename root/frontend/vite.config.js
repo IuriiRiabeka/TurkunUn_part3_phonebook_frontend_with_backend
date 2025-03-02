072609a9e1ed7b4d4fac9ac62cc74152c11433a8
IuriiRiabeka <sam2fly7@gmail.com>
2025-03-02 11:26:33 +0200

    upload project

--- root/frontend/vite.config.js
+++ root/frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
+  root: '.',  // або 'frontend', якщо ви запускаєте з кореня
+  build: {
+    outDir: 'dist',
+  },
    server: {
     proxy: {
       '/api': {
